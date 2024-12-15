b6b447ade47d85dbc3d084511bc7e946b888eb9e
serigio04 <serigi.gomar15@gmail.com>
2024-12-14 23:17:32 -0600

    [ADD] estructura básica

---
+++ src/Routes/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "../pages/Home";
+
+const AppRoutes = () => (
+    <Routes>
+        {/* <Route excat path="/peliculas"/> */}
+        <Route path="/" element={Home}/>
+    </Routes>
+);
+
+export default AppRoutes;
