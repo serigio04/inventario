03acb5faaa2e4989d56421f92eb8af7695edc719
serigio04 <serigi.gomar15@gmail.com>
2025-03-31 22:17:59 -0600

    [UPDATE] se actualizaron las rutas ya que se movieron al backend

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import Header from "./components/Header";
-import AppRoutes from "./Routes";
+import AppRoutes from "../backend/Routes";
 
 
 const App = () => {
