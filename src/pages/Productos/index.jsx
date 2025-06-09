c5e42cb4e41691c317dc25b97e820ed731eed536
serigio04 <serigi.gomar15@gmail.com>
2025-06-08 23:36:10 -0600

    [UPDATE] Se actualizo la pagina de usuarios

--- src/pages/Productos/index.jsx
+++ src/pages/Productos/index.jsx
@@ -3,6 +3,8 @@ import * as React from "react";
 import { DataGrid } from '@mui/x-data-grid';
 import { Paper } from "@mui/material";
 import { getProductos } from "../../services/productService";
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
 
 const columns = [
     {field: 'id', headerName: 'ID', width: 70},
@@ -24,15 +26,25 @@ const Productos = () => {
         fetchProductos();
     }, []);
 
+    const paginationModel = { page: 0, pageSize: 10 };
+
     return (
-        <div style={{display: 'flex', justifyContent: 'center', alignContent: 'center'}}>
-            <Paper sx={{height: 400, width: "80%", padding: '1%'}}>
+        <div style={{display: 'flex', justifyContent: 'center', alignContent: 'center', flexDirection: 'column'}}>
+        <Stack spacing={2} direction="row">
+            <Button variant="contained" >
+                Descargar
+            </Button>
+        </Stack>
+        <div style={{display: 'flex', justifyContent: 'center', alignContent: 'center', marginTop: '3vh'}}>
+            <Paper sx={{height: "77vh", width: "80%", padding: '1%'}}>
                 <DataGrid 
                     rows={rows} 
-                    columns={columns} 
+                    columns={columns}
+                    initialState={{ pagination: { paginationModel } }} 
                     pageSizeOptions={[5, 10]}
                 />
             </Paper>
+        </div>
         </div>
     )
 }
