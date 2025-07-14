3cb6ee378a410b373b153e37b2edfa5b16c0748d
serigio04 <serigi.gomar15@gmail.com>
2025-07-13 22:49:26 -0600

    [FIX] Se arreglo el uso del endpoint de login y la ruta privada para manejar el token

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./pages/Home";
 import UsuariosTable from "./pages/Usuarios";
 import Productos from "./pages/Productos"
 import { RegistrarForm } from "./pages/Sesion";
 import LoginForm from "./pages/Sesion/login";
+import RutaPrivada from "./pages/Sesion/rutaPrivada";
 
 
 const App = () => {
@@ -13,11 +14,22 @@ const App = () => {
     <BrowserRouter >
       <Header />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/usuarios" element={<UsuariosTable/>} /> 
-        <Route path="/productos" element={<Productos />}/>
+        <Route path="/" element={
+          <RutaPrivada>
+            <Home />
+          </RutaPrivada>} />
+        <Route path="/usuarios" element={
+          <RutaPrivada>
+            <UsuariosTable/>
+          </RutaPrivada>} /> 
+        <Route path="/productos" element={
+          <RutaPrivada>
+            <Productos />
+          </RutaPrivada>} />
         <Route path="/registrar" element={<RegistrarForm />}/>
         <Route path="/login" element={<LoginForm />}/>
+
+        <Route path="*" element={<Navigate to="/login" />} />
       </Routes>
     </BrowserRouter>
   )
